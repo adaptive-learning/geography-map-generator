775f99597e3c2a59fa9426a76c1983108aee5f1c
Vit Stanislav <slaweet@seznam.cz>
2014-02-22 09:58:33 +0100

    Create script to download source shapefiles

---
+++ fetch_shapefiles.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import urllib
+from zipfile import ZipFile
+
+
+urls = [
+    "http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_countries_lakes.zip",
+    "http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries_lakes.zip",
+    "http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/ne_110m_populated_places.zip",
+    "http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_populated_places.zip",
+    "http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_1_states_provinces_lakes.zip",
+    "http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/physical/ne_50m_land.zip",
+    "http://biogeo.ucdavis.edu/data/gadm2/shp/ITA_adm.zip",
+    "http://biogeo.ucdavis.edu/data/gadm2/shp/ESP_adm.zip",
+    "http://biogeo.ucdavis.edu/data/gadm2/shp/FRA_adm.zip",
+]
+
+
+def main():
+    for url in urls:
+        print url
+        name, headers = urllib.urlretrieve(url)
+        zp = ZipFile(name)
+        zp.extractall("src/" + url[url.rfind('/'):-4])
+
+if __name__ == '__main__':
+    main()
