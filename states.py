0a7027ffc34f107928f914dabd5bfd469b8186e4
Vit Stanislav <slaweet@seznam.cz>
2016-01-17 08:30:56 +0100

    Add Slovak and more Czech rivers

--- states.py
+++ states.py
@@ -16,6 +16,8 @@ CZECH_CITIES_FILE = "src/czech-republic-latest.shp/places.shp"
 SLOVAK_CITIES_FILE = "src/slovakia-latest.shp/places.shp"
 CZECH_MOUNTAINS = "my_src/CZE_mountains/CZE_mountains.shp"
 CZECH_RIVERS_FILE = "my_src/CZE_rivers//CZE_rivers.shp"
+CZECH_RIVERS_FILE = "my_src/reky_cr/reky_cr.shp"
+SLOVAK_RIVERS_FILE = "my_src/reky_sr/reky_sr.shp"
 
 
 def cities_size_filter(record):
@@ -62,7 +64,7 @@ class StateGenerator(SingleMapGenerator):
         return BIG_CITIES_FILE
 
     def get_bg_src(self):
-        if self.code in ["US", "CA", "CZ"]:
+        if self.code in ["US", "CA", "CZ", "SK"]:
             return COUNTRIES_MEDIUM_FILE
         else:
             return COAST_FILE
@@ -294,6 +296,13 @@ class CzSkGenerator(StateGenerator):
         ]}
         return filter
 
+    def hacky_fixes(self, map_data):
+        map_data = map_data.replace(
+            '<g class="" id="bg">',
+            '<g class="" id="bg">' +
+            '<path d="M-10000.0,-10000.0L-10000.0,10000.0L10000.0,10000.0L10000.0,-10000.0Z "/>')
+        return map_data
+
 
 class CzechGenerator(CzSkGenerator):
     def get_cities_src(self):
@@ -301,7 +310,7 @@ class CzechGenerator(CzSkGenerator):
 
     def get_config(self):
         config = super(CzSkGenerator, self).get_config()
-        config["layers"][0]["filter"] = ["iso_a2", "is", "CZ"]
+        config["layers"][0]["filter"] = ["iso_a2", "in", ["CZ"]]
         config["layers"].append({
             "id": "mountains",
             "src": CZECH_MOUNTAINS,
@@ -314,23 +323,11 @@ class CzechGenerator(CzSkGenerator):
             "id": "river",
             "src": CZECH_RIVERS_FILE,
             "attributes": {
-                "code": "name",
-                "name": "name"
+                "code": "NAZ_TOK",
+                "name": "NAZ_TOK"
             },
         })
         return config
-
-    def hacky_fixes(self, map_data):
-        map_data = map_data.replace(
-            "M398.077452,40.651030L",
-            "M-5000.0,-5000.0L-5000.0,5000.0L5000.0,5000.0L5000.0,-5000.0L-5000.0,-5000.0Z " +
-            "M398.077452,40.651030L")
-        #map_data = map_data.replace(
-        #    "M0.000000,0.000000L0.000000,567.267337" +
-        #    "L1000.000000,567.267337L1000.000000,0.000000L0.000000,0.000000Z",
-        #    "M-5000.0,-5000.0L-5000.0,5000.0L5000.0,5000.0L5000.0,-5000.0L-5000.0,-5000.0Z")
-        # map_data = map_data.decode('cp1252').encode('utf')
-        return map_data
 
 
 class SlovakiaGenerator(CzSkGenerator):
@@ -339,15 +336,18 @@ class SlovakiaGenerator(CzSkGenerator):
 
     def get_config(self):
         config = super(CzSkGenerator, self).get_config()
+        config["layers"][0]["filter"] = ["iso_a2", "in", ["SK"]]
+        config["layers"][0]["simplify"] = 0
         config["bounds"]["padding"] = 0.01
+        config["layers"].append({
+            "id": "river",
+            "src": SLOVAK_RIVERS_FILE,
+            "attributes": {
+                "code": "name",
+                "name": "name"
+            },
+        })
         return config
-
-    def hacky_fixes(self, map_data):
-        map_data = map_data.replace(
-            "M0.000000,0.000000L0.000000,510.450681" +
-            "L1000.000000,510.450681L1000.000000,0.000000L0.000000,0.000000Z",
-            "M-5000.0,-5000.0L-5000.0,5000.0L5000.0,5000.0L5000.0,-5000.0L-5000.0,-5000.0Z")
-        return map_data
 
 
 class GermanyGenerator(StateGenerator):
